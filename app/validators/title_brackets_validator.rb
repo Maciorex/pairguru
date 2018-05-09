e564a2d750cd469b89c65940a8b3652d8e9ece22
Maciorex <maciej.zabski@gmail.com>
2018-05-09 19:36:08 +0200

    create custom title bracket validator

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -3,5 +3,26 @@ class TitleBracketsValidator < ActiveModel::Validator
     if record.title.blank?
       record.errors[:base] << "Title is blank"
     end
+
+    stack  = []
+    lookup = { '(' => ')', '[' => ']', '{' => '}' }
+    left   = lookup.keys
+    right  = lookup.values
+    brackets = %w[ () [] {} ]
+
+    record.title.each_char do |char|
+      if left.include? char
+        stack << char
+      elsif right.include? char
+        return record.errors[:base] << "There is something wrong with brackets" if stack.empty? || (lookup[stack.pop] != char)
+      end
+    end
+    unless stack.empty?
+      return record.errors[:base] << "There is something wrong with brackets"
+    end
+
+    unless  brackets.none? { |b| record.title.include?( b ) }
+      return record.errors[:base] << "Brackets are empty"
+    end
   end
 end
