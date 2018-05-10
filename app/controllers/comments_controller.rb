2dd7fa26d8ba4f76585aae35836843734e35657f
Maciorex <maciej.zabski@gmail.com>
2018-05-10 13:28:02 +0200

    create comment form, render it from movie show view, update create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,27 @@
-class CommentsController < ApplicationRecord
-
-  def new
-    @comment = Commment.new
-  end
-
+class CommentsController < ApplicationController
+before_action :find_movie
   def create
+
+    @comment = @movie.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+
+    if @comment.save
+      flash[:success] = "Successfully created a comment"
+      redirect_to movie_path(@movie)
+    else
+      flash[:danger] = "Your comment is invalid, check it again"
+      redirect_to movie_path(@movie)
+    end
   end
 
+  private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+
+    def find_movie
+      @movie = Movie.find(params[:movie_id])
+    end
 end
