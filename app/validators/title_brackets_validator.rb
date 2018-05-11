29b50165b54db5c4f8aa7f797fc783f23b9b40bd
Maciorex <maciej.zabski@gmail.com>
2018-05-11 14:08:24 +0200

    fix styling issues

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -1,27 +1,27 @@
 class TitleBracketsValidator < ActiveModel::Validator
-  def validate(record)
-    if record.title.blank?
-      record.errors[:base] << "Title is blank"
-    end
 
-    stack  = []
+  def validate(record)
+    stack = []
     lookup = { '(' => ')', '[' => ']', '{' => '}' }
-    left   = lookup.keys
-    right  = lookup.values
+    left = lookup.keys
+    right = lookup.values
     brackets = %w[ () [] {} ]
 
     record.title.each_char do |char|
       if left.include? char
         stack << char
       elsif right.include? char
-        return record.errors[:base] << "There is something wrong with brackets" if stack.empty? || (lookup[stack.pop] != char)
+        if stack.empty? || (lookup[stack.pop] != char)
+          return record.errors[:base] << "There is something wrong with brackets"
+        end
       end
     end
+    
     unless stack.empty?
       return record.errors[:base] << "There is something wrong with brackets"
     end
 
-    unless  brackets.none? { |b| record.title.include?( b ) }
+    unless brackets.none? { |br| record.title.include?(br) }
       return record.errors[:base] << "Brackets are empty"
     end
   end
